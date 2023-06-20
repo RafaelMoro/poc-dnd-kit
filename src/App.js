9da1dce1aff187d1a86c5680baf21ce4449efe1d
Rafael Moro <rafael.moro.galindo@gmail.com>
2023-06-20 09:56:38 -0500

    [ADD] - Putting the demo files in a demo folder inside src/components. Installing dependency of babel due CRA warning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import { DndContext } from '@dnd-kit/core';
 // import { List } from './components/List';
-import { Main } from "./components/Main";
+import { Main } from "./components/demo/Main";
 
 function App() {
   return (
